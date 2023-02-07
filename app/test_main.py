1bf6c74ee49794553c6584720648fe486d11d007
koraypoyraz <koraypoyraz1@gmail.com>
2023-02-07 20:55:48 +0100

    added simple tests

--- app/test_main.py
+++ app/test_main.py
@@ -5,17 +5,17 @@ from .main import app
 client = TestClient(app)
 
 
-# def test_root():
-#     response = client.get("/")
-#     assert response.status_code == 200
-#     assert response.json() == {"message": "Hello World"}
-#
-#
-# def test_say_hello():
-#     name = 'Py'
-#     response = client.get(f"/hello/{name}")
-#     assert response.status_code == 200
-#     assert response.json() == {"message": f"Hello {name}"}
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+
+def test_say_hello():
+    name = 'Py'
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.json() == {"message": f"Hello {name}"}
 
 # def test_get_images():
 #     assert False
