3c2d918b4c52cdb7c606aa3e92b5b13d81ad8d6b
koraypoyraz <koraypoyraz1@gmail.com>
2023-06-26 11:20:56 +0200

    - fix testing

--- app/tests/test_images.py
+++ app/tests/test_images.py
@@ -1,20 +1,24 @@
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import pytest
+from sqlalchemy.pool import StaticPool
 import random
 
-from app.main import app, title
+from app.main import app
 from app.core.dependencies import get_db
 from app.core.database import Base
 from app.core.settings import DATABASE_TEST_URI
-from . import fake_fact
+from app.tests import fake_obj
 
 engine = create_engine(
-    DATABASE_TEST_URI, connect_args={"check_same_thread": False}
+    DATABASE_TEST_URI,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool
 )
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
 
 
 def override_get_db():
@@ -25,13 +29,6 @@ def override_get_db():
         db.close()
 
 
-@pytest.fixture()
-def test_db():
-    Base.metadata.create_all(bind=engine)
-    yield
-    Base.metadata.drop_all(bind=engine)
-
-
 app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
@@ -39,24 +36,11 @@ client = TestClient(app)
 base_url = '/images'
 
 
-def test_root(test_db):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": title}
-
-
-def test_say_hello(test_db):
-    name = 'Py'
-    response = client.get(f"/hello/{name}")
-    assert response.status_code == 200
-    assert response.json() == {"message": f"Hello {name}"}
-
-
-def test_get_images(test_db):
+def test_get_images():
     expected = [client.post(
         base_url,
         json=fake
-    ) for fake in fake_fact.generate()]
+    ) for fake in fake_obj.generate()]
 
     response = client.get(base_url)
 
@@ -64,10 +48,10 @@ def test_get_images(test_db):
     assert len(response.json()) == len(expected)
 
 
-def test_get_image(test_db):
+def test_get_image():
     expected = client.post(
         base_url,
-        json=random.sample(fake_fact.generate(), 1)[0]
+        json=random.sample(fake_obj.generate(), 1)[0]
     ).json()
 
     response = client.get(base_url + f'/{expected["id"]}')
@@ -76,8 +60,8 @@ def test_get_image(test_db):
     assert response.json() == expected
 
 
-def test_create_image(test_db):
-    expected = random.sample(fake_fact.generate(), 1)[0]
+def test_create_image():
+    expected = random.sample(fake_obj.generate(), 1)[0]
 
     response = client.post(
         base_url,
