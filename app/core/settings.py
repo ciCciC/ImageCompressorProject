a5023435feaec33e29daaa5143aaffb52f1e636f
koraypoyraz <koraypoyraz1@gmail.com>
2023-06-26 12:01:56 +0200

    - settings default values

--- app/core/settings.py
+++ app/core/settings.py
@@ -5,9 +5,10 @@ config = {
     **dotenv_values(".env.test")
 }
 
-HOST = config.get('HOST')
-PORT = config.get('PORT')
-DATABASE_URI = config.get('DATABASE_URI')
+# DEV
+HOST = config.get('HOST', "127.0.0.1")
+PORT = config.get('PORT', 5000)
+DATABASE_URI = config.get('DATABASE_URI', "sqlite:///data\images_db")
 
 # TEST
-DATABASE_TEST_URI = config.get('DATABASE_TEST_URI')
+DATABASE_TEST_URI = config.get('DATABASE_TEST_URI', "sqlite://")
